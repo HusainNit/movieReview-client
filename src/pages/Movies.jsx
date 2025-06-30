066bb55ab35fe0ce9d55336cf89c3eb3cf3a0516
GA35a <ga35a35@email.com>
2025-06-30 19:52:03 +0300

    added to the favorite function

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,8 @@
 import { POSTER_PATH } from "../globals";
 import { useState, useEffect } from "react";
 import { GetMovies } from "../services/MoviesGetter";
+import { addFavorite } from '../services/favoriteApi';
+
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
@@ -14,6 +16,28 @@ const Movies = () => {
   const unSelect = () => {
     setSelectedMovie(null);
   };
+
+  const handleAddToFavorites = async (movie) => {
+    const userId = localStorage.getItem('userId')
+    if (!userId) {
+      alert("Please sign in first to add favorites")
+      return;
+    }
+
+    try {
+      await addFavorite(userId, {
+      movieId: movie.id,
+      movieTitle: movie.title,
+      movieType: movie.genre_ids?.[0] || "Unknown",
+      moviePoster: `${POSTER_PATH}${movie.backdrop_path}` 
+    });
+    alert("Movie added to favorites!");
+    } catch (error) {
+      console.error("Error adding to favorites:", error.message);
+      alert("Failed to add favorite. Maybe it's already added.");
+    }
+
+  }
 
   useEffect(() => {
     const handleMovies = async () => {
