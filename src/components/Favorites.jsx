353c36eb1a62d9806e6e7ffff7c172142d418c6e
GA35a <ga35a35@email.com>
2025-06-30 18:42:31 +0300

    add favoriteApi.js and Favorites.jsx

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { getFavorites, removeFavorite } from '../services/favoriteApi';
+
+const Favorites = ({ userId }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    async function fetchFavorites() {
+      const data = await getFavorites(userId);
+      setFavorites(data);
+    }
+    fetchFavorites();
+  }, [userId]);
+
+  const handleRemove = async (favoriteId) => {
+    await removeFavorite(userId, favoriteId);
+    setFavorites(favorites.filter(fav => fav._id !== favoriteId));
+  }
+
+    return (
+    <div>
+      <h2>My Favorite Movies</h2>
+      <ul>
+        {favorites.map(fav => (
+          <li key={fav._id}>
+            {fav.movieId.title}
+            <button onClick={() => handleRemove(fav._id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Favorites;
